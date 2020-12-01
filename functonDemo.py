0d67d464219d5e16664d334f88aac2d90a8f3d07
alejandrorebolledo <54950946+alejandrorebolledo@users.noreply.github.com>
2020-12-01 12:25:13 -0800

    Create functonDemo.py

---
+++ functonDemo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# functonDemo.py
+def printStars(n):
+    print(n," ",end="")
+    print("* * * * * * * * * * * * * *")
+    print("* * * * * * * * * * * * * *")
+    print("* * * * * * * * * * * * * *")
+    
+def main():
+    printStars(1)# calling a function printStars()
+    printStars(2)
+    printStars(3)
+    
+main()
+
+'''
+
+python functions
+- defining a function
+- calling a function 
+- function arguments (paramaters)
+'''
